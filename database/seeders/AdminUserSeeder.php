4d293803d5027f37fc4046f6448a05bc6003504f
vax-02 <javier.xcn.18@gmail.com>
2026-05-17 21:06:21 -0400

    v1.1

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('12345678'),
+            'rol' => 1,
+        ]);
+    }
+}
